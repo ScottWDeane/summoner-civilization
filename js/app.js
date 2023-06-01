840dc406dad76c8fda2ba5136f137760cdd71835
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-01 00:29:18 -0700

    implement basic resources and resource collection
    build out basic four resources, "soul energy", "bones", "blood", and "corpses."
    build out basic resource collection buttons, link to basic collection logic.
    add maximum capacity to each of the four basic resources.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,45 @@
 var cookies = 0;
 var cursors = 0;
+var soulEnergy = 0;
+var soulCap = 10;
+var bones = 0;
+var bonesCap = 10;
+var blood = 0;
+var bloodCap = 10;
+var corpses = 0;
+var corpseCap = 10;
 
-var save = {
-    cookies: cookies,
-    cursors: cursors
-}
+// var save = {
+//     cookies: cookies,
+//     cursors: cursors
+// }
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function collectEnergy(number){
+    if (soulEnergy < soulCap) {
+        soulEnergy = soulEnergy + number;
+        document.getElementById("soulEnergy").innerHTML = soulEnergy;
+    }
+};
+
+function collectBones(number){
+    if (bones < bonesCap) {
+        bones = bones + number;
+        document.getElementById("bones").innerHTML = bones;
+    }
+};
+
+function collectBlood(number){
+    if (blood < bloodCap) {
+        blood = blood + number;
+        document.getElementById("blood").innerHTML = blood;
+    }
+};
+
+function collectCorpses(number){
+    if (corpses < corpseCap) {
+        corpses = corpses + number;
+        document.getElementById("corpses").innerHTML = corpses;
+    }
 };
 
 function buyCursor(){
@@ -23,9 +54,9 @@ function buyCursor(){
     document.getElementById("cursorCost").innerHTML = nextCost; // update cursor cost for the user
 }
 
-function save() {
-    localStorage.setItem("save",JSON.stringify(save));
-}
+// function save() {
+//     localStorage.setItem("save",JSON.stringify(save));
+// }
 
 window.setInterval(function(){
     cookieClick(cursors);
