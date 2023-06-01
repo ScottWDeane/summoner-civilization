9bc7389b982f38083bceafd81f9558920badbfb4
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-01 00:40:41 -0700

    remove "cursor" code
    remove unused boilerplate "cursor" code

--- js/app.js
+++ js/app.js
@@ -42,16 +42,7 @@ function collectCorpses(number){
     }
 };
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.2,cursors));    //calc cost of next cursor
-    if (cookies >= cursorCost) {                                // check if player can actually afford the cursor
-        cursors = cursors + 1;                                  // increment number of cursors
-        cookies = cookies - cursorCost;                         // remove cost of cursor upgrade from current cookies count
-        document.getElementById('cursors').innerHTML = cursors  // update number of current cursors user has
-        document.getElementById('cookies').innerHTML = cookies  // update current cookie count to reflect purchase
     };
-    var nextCost = Math.floor(10 * Math.pow(1.2,cursors));      // calculate cost of next cursor
-    document.getElementById("cursorCost").innerHTML = nextCost; // update cursor cost for the user
 }
 
 // function save() {
