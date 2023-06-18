d5bc967a55ef6bfa0d52cf2a5fd59b0e1000245d
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-17 20:39:57 -0700

    convert summon skeleton button
    add HTML button and the requisite logic for summoning skeletons to Vue.js version.

--- SummonerCivilization.js
+++ SummonerCivilization.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      // basic resources
       soulEnergy: 0,
       soulCap: 100,
       bones: 0,
@@ -9,14 +10,20 @@ const app = Vue.createApp({
       stoneCap: 50,
       corpses: 0,
       corpseCap: 50,
+      // used for manual gathering logic
       isGatheringSouls: false,
       isGatheringBones: false,
       isGatheringStone: false,
       isGatheringCorpses: false,
+      // skeleton laborers
+      skeletons: 0,
+      skeletonsCap: 1,
+      nextSkeletonCost: 0,
     }
   },
   async created() {
     setInterval(this.collectCurrentManualResource, 100);
+    this.calcSummonSkeletonCost();
   },
   methods: {
     setGatherResource(currentResource) {
@@ -85,6 +92,18 @@ const app = Vue.createApp({
       this.isGatheringBones = false;
       this.isGatheringStone = false;
       this.isGatheringCorpses = false;
+    },
+    summonSkeleton() {
+      this.calcSummonSkeletonCost();
+      if (this.bones >= this.nextSkeletonCost && this.skeletons < this.skeletonsCap) {
+        this.skeletons = this.skeletons + 1;
+        this.bones = this.bones - this.nextSkeletonCost;
+      }
+      this.calcSummonSkeletonCost();
+    },
+    calcSummonSkeletonCost() {
+      this.nextSkeletonCost = Math.floor(10 * Math.pow(1.1, this.skeletons));
+
     }
   }
 });
