a223da4b8fc396cee89258b345f074ab0204aea5
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-21 18:57:10 -0700

    change Research tooltips to have <hr> to split lore text and bonus description
    Add a <hr> tag for all research descriptions to separate the lore blurb and the actual mechanical change.
    Instead of displaying the research.description as the content of a div, use vue v-html="research-description" so we can treat the description text as HTML, allowing us to use HTML tags in the text.
    We use this functionality to display a horizontal line inside the description text, saving us from needing a separate data property for research.

--- components/ResearchDisplay.js
+++ components/ResearchDisplay.js
@@ -9,7 +9,7 @@ app.component('research-display', {
                         'soulEnergy': 100,
                         'bones': 100
                     },
-                    description: 'Refine your resurrection spell so that skeletons have both of their hands. Most of the time.',
+                    description: 'Refine your resurrection spell so that skeletons have both of their hands. Most of the time. <hr> Double all skeleton labor production.',
                     bonus: 2, // double all skeletons' labor
                     requirements: {
                         owned_settlements: 0
@@ -22,7 +22,7 @@ app.component('research-display', {
                         'soulEnergy': 100,
                         'stone': 100
                     },
-                    description: 'Instruct your skeletons to build walls for the Rock Piles, increasing the amount of rocks that can be stored per pile.',
+                    description: 'Instruct your skeletons to build walls for the Rock Piles, increasing the amount of rocks that can be stored per pile. <hr> +50% storage capacity for Rock Piles.',
                     bonus: 1.5, // 50% bonus to rock pile storage capacity
                     requirements: {
                         owned_settlements: 0
@@ -36,7 +36,7 @@ app.component('research-display', {
                         'bones': 500,
                         'corpses': 500
                     },
-                    description: 'Draw power from your skeletons and consume the middling life energy of some... reagants... and complete a ritual to empower your staff. Increase your maximum capacity for Soul Energy.',
+                    description: 'Draw power from your skeletons and consume the middling life energy of some reagants to empower your staff. <hr> 500% increased maximum Soul Energy cap.',
                     bonus: 5, // 500% bonus (or, 5x) to maximum soul energy cap
                     requirements: {
                         owned_settlements: 0,
@@ -53,11 +53,10 @@ app.component('research-display', {
         <div> Research and Upgrades </div>
         <ul>
             
-            <button v-for="(research, index) in available_research"> 
+            <button v-for="(research, index) in available_research" class="tooltip button"> 
             {{ research.title }} 
-            <div>
-                {{ research.description }}
-            </div>
+            <span class="tooltiptext" v-html="research.description">
+            </span>
             </button>
         </ul>
         <div>--------------------------------------------------------------------</div>
